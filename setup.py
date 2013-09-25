97193212ce99001a5121710ed1f411701a457eec
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2013-09-25 00:52:45 -0430

    [IMP] setup.py now let to build and install the package.

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 from distutils.core import setup
+try:
+    # 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    # 2.x
+    from distutils.command.build_py import build_py
+
+cmdclass = {'build_py': build_py}
+command_options = {}
 
 setup(
     name='OERP MODULE',
@@ -10,6 +21,9 @@ setup(
     #~ url='http://pypi.python.org/pypi/.../',
     #~ license='LICENSE.txt',
     description='Creating updating openerp module tool.',
+    #~ keywords= define keywords,
     long_description=open('README.rst').read(),
     #~ install_requires=[],
+    cmdclass=cmdclass,
+    command_options=command_options,
 )
