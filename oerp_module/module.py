3256243d724d0e7d09b19d9240e5eb1c8ef7ad11
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2013-09-25 01:12:06 -0430

    [FIX] at license message use of www.vauxoo.com instead of openerp.com.ve

--- oerp_module/module.py
+++ oerp_module/module.py
@@ -17,7 +17,7 @@ class Template(object):
 # -*- encoding: utf-8 -*-
 ###############################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
-#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
 #    All Rights Reserved
 ############# Credits #########################################################
 #    Coded by: %s
