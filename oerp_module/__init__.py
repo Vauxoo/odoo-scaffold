2885a1b004a8b57fdfc246ec98da7033fe5ab990
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-03-12 03:02:55 -0430

    [IMP] separate the branch class into a new py file.

--- oerp_module/__init__.py
+++ oerp_module/__init__.py
@@ -1,7 +1,8 @@
 import module
 import config
+import branch 
 from module import Module
 from module import Config
-from module import Branch 
+from branch import Branch 
 
 _oerp_version_list = ['6.0', '6.1', '7.0']
