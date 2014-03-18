581c52474c5ebe23ed68d965a17a2c73152f9d75
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-03-18 11:05:17 -0430

    [IMP] move the method run from the script to the oerpmodule lib definition.

--- oerp_module/__init__.py
+++ oerp_module/__init__.py
@@ -4,3 +4,34 @@ import branch
 from module import Module
 from config import Config
 from branch import Branch 
+
+def run(args, config):
+    """
+    run the corresponding action
+    """
+    if args.action == 'config':
+        args.list_repositories and config.print_repositories()
+    else:
+        module = Module(
+            args.module_name, args.module_developers, args.module_planners,
+            args.module_auditors, folder=args.destination_folder)
+
+        if args.action == 'branch':
+            branch = Branch(
+                module, args.branch_suffix, args.parent_repo,
+                args.oerp_version, args.destination_folder)
+            branch.create_branch()
+            module.update_path(branch)
+            module.create_main_directory()
+            module.create_directories()
+            module.create_base_files()
+        elif args.action == 'create':
+            module.create_main_directory()
+            module.create_directories()
+            module.create_base_files()
+
+        elif args.action == 'append':
+            module.create_py_files(args.append_file, args.file_name)
+
+        #~ module.branch_changes_apply()
+    return True
