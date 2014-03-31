8acd605c2e277d533d4f90286cefd29fdf83ca5f
Katherine Zaoral <katherine.zaoral@vauxoo.com>
2014-03-31 12:22:28 -0430

    [IMP] move argument methods from config class to be only simple functions in the init file of the python module. another small changes were make.

--- oerp_module/__init__.py
+++ oerp_module/__init__.py
@@ -5,13 +5,156 @@ from module import Module
 from config import Config
 from branch import Branch 
 
+import argparse
+import argcomplete
+
+def argument_parser():
+    """
+    This function create the help command line and manage and filter the
+    parameters of this program (default values, choices values)
+    """
+    my_config = Config()
+    parser = argparse.ArgumentParser(
+        prog='oerpmodule',
+        description='Create new openerp module structure and basic files.',
+        epilog="""
+Openerp Developer Comunity Tool
+Development by Vauxoo Team (lp:~vauxoo)
+Coded by Katherine Zaoral <kathy@vauxoo.com>.
+Source code at lp:~katherine-zaoral-7/+junk/oerp_module.""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    subparsers = parser.add_subparsers(
+        description=('Valid actions over a module than can be done with'
+            ' oerpmodule.'),
+        dest='action',
+        help='subcommands help')
+
+    # create sub parser for create action
+    create_parser = subparsers.add_parser(
+        'create',
+        help='Inicializate module. Create new directory with basic files.')
+    create_paser = add_common_options(create_parser, my_config)
+
+    # create sub parser for branch action
+    branch_parser = subparsers.add_parser(
+        'branch',
+        help='Create a new module using a branch.')
+    branch_paser = add_common_options(branch_parser, my_config)
+
+    branch_parser.add_argument(
+        '-r', '--parent_repo',
+        metavar='PARENT_REPO',
+        type=str,
+        required=True,
+        choices=my_config.get_repositories_names(),
+        help=('The name of repository from will be create the new module. To '
+              'look the repository list use oerpmodule config -l.'))
+
+    branch_group = branch_parser.add_argument_group('New Branch Name options', (
+        'This way you can configure the new branch name.'))
+
+    branch_group.add_argument(
+        '-s', '--branch-suffix',
+        metavar='DEVELOPER_ACRONYM',
+        type=str,
+        help=str('Generally developer acronym name. It use when creating the'
+                 ' branch for identify the team user owner in a simply way,'
+                 ' It use is recommended.'))
+    branch_group.add_argument(
+        '-ov', '--oerp-version',
+        metavar='VERSION',
+        type=str,
+        choices=my_config._oerp_version_list,
+        help='Openerp version number')
+
+    branch_parser.set_defaults(
+        oerp_version='7.0',
+        parent_repo='addons-vauxoo')
+
+    # create sub parser for append action
+    append_parser = subparsers.add_parser(
+        'append', 
+        help='Append a file to the module.')
+    append_paser = add_common_options(append_parser, my_config)
+
+    append_parser.add_argument(
+        'append_file',
+        metavar='FILE_TYPE',
+        type=str,
+        choices=['model', 'wizard'],
+        help='The type of file you want to append.')
+    append_parser.add_argument(
+        'file_name',
+        metavar='FILE_NAME',
+        type=str,
+        help='the name of the new model or wizard file that will be created.')
+
+    # create sub parser for config action
+    config_parser = subparsers.add_parser(
+        'config', 
+        help='Set and check the oerpmodule config')
+    config_parser.add_argument(
+        '-l', '--list-repositories', action='store_true',
+        help='List the configurate repositories.')
+
+    argcomplete.autocomplete(parser)
+    return parser.parse_args().__dict__
+
+def add_common_options(subparser, my_config):
+    """
+    I recive a parser object that repersent a subparser in this script to add
+    it the common options needed. The subparser that share this common options
+    are [create, append, branch] subparsers. The common options are:
+    [module_name, module_developers, module_planners, module_auditors, dir].
+    @return: the subparser object with the common arguments added.
+    """
+    subparser.add_argument(
+        'module_name',
+        metavar='MODULE_NAME',
+        type=str,
+        help='name of the module to create.')
+    subparser.add_argument(
+        '-d', '--module-developers',
+        metavar='DEVELOPERS_INFO',
+        type=str,
+        help=str('A string with the module developers information. The format'
+                 ' of this string is \'First Developer Name'
+                 ' <developer@mail.com>, Second Developer Name'
+                 ' <developerX@mail.com>\''),
+        required=True)
+    subparser.add_argument(
+        '-p', '--module-planners',
+        metavar='PLANNERS_INFO',
+        type=str,
+        help=str('A string with the module planners information. The format'
+                 ' of this string is \'First Planner Name'
+                 ' <planner@mail.com>, Second Planner Name'
+                 ' <plannerX@mail.com>\''),
+        required=True)
+    subparser.add_argument(
+        '-t', '--module-auditors',
+        metavar='AUDITORS_INFO',
+        type=str,
+        help=str('A string with the module auditors information. The format'
+                 ' of this string is \'First Auditor Name'
+                 ' <auditor@mail.com>, Second Auditor Name'
+                 ' <auditorX@mail.com>\''),
+        required=True)
+    subparser.add_argument(
+        '-dir', '--destination-folder',
+        metavar='DIR',
+        type=str,
+        default=my_config.get_current_path(),
+        help='name of the folder where to put the module')
+    return subparser
+
 def run(args):
     """
     run the corresponding action
     """
+    my_config = Config()
     if args['action'] == 'config':
-        Config()
-        args['list_repositories'] and Config().print_repositories()
+        args['list_repositories'] and my_config.print_repositories()
     else:
         module = Module(
             args['module_name'], args['module_developers'],
@@ -60,9 +203,7 @@ def confirm_run(args):
     return True
 
 def main():
-
-    config = Config()
-    args = config.argument_parser().__dict__
+    args = argument_parser()
     confirm_run(args)
     run(args)
 
